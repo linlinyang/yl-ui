9f0eed2112f22e4ef0952049ec5544d1760964c4
linlinyang <935704186@qq.com>
2019-04-18 17:45:26 +0800

    switch create

---
+++ packages/components/Switch/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="wrapClasses">
+        <input type="hidden" :name='name' :value='currentValue'>
+        <div class=""></div>
+    </div>
+</template>
+
+<script>
+
+const prefixCls = 'yl-ui-switch';
+
+export default {
+    name: 'switch',
+    props: {
+        value:{
+            type: [Boolean,Number,String],
+            default: false
+        },
+        disabled:{
+            type: Boolean,
+            default: false
+        },
+        activeText: String,
+        inActiveText: String
+    }
+}
+</script>
+
