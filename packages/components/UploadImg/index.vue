7752f1b82b9b41f64f3816a529237dace5408878
linlinyang <935704186@qq.com>
2019-05-30 16:43:46 +0800

    upload img build

---
+++ packages/components/UploadImg/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div ></div>
+</template>
+
+<script>
+import JSCropper from 'JSCropper';
+const prefixCls = 'yl-ui-upload-img';
+
+export default {
+    name: 'UploadImg',
+    props: {
+        
+    },
+    data(){
+        return {};
+    }
+}
+</script>
