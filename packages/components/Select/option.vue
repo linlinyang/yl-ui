79accccff59186022253259aac677747dcb074bb
linlinyang <935704186@qq.com>
2019-06-04 16:56:06 +0800

    option modify

--- packages/components/Select/option.vue
+++ packages/components/Select/option.vue
@@ -1,12 +1,26 @@
 <template>
-    <div class=""></div>
+    <div 
+        :class="classes"
+        @click="select"
+    >
+        <slot>{{ displayLabel }}</slot>
+        <Icon
+            v-if='selected'
+            type='correct'
+            color='rgba(45,140,240,.9)'
+        ></Icon>
+    </div>
 </template>
 
 <script>
+import Icon from '#c/Icon/';
 const prefixCls = 'yl-ui-option';
 
 export default {
     name: 'Option',
+    components: {
+        Icon
+    },
     props: {
         value: {
             type: [String,Number],
@@ -22,9 +36,29 @@ export default {
         },
         label: [String,Number]
     },
-    data(){
-        return {};
+    computed: {
+        displayLabel(){
+            return this.label ? this.label : this.value;
+        },
+        classes(){
+            return [
+                prefixCls,
+                {
+                    [`${prefixCls}-selected`]: this.selected,
+                    [`${prefixCls}-disabled`]: this.disabled
+                }
+            ];
+        }
     },
+    methods: {
+        select(e){
+            if(this.disabled){
+                return ;
+            }
+
+            this.$emit('on-select',this.value);
+        }
+    }
     
 }
 </script>
